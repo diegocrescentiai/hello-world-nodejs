dc171b8a0e3b66fb17fc41af52c106431247da35
diegocrescenti70 <diego.crescenti70@gmail.com>
2025-10-07 18:21:40 +0200

    Progetto Node.js Hello World con Express v_02

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello, World!</h1>');
+  res.send('<h1>Hello, World! v_02</h1>');
 });
 
 app.listen(PORT, () => {
